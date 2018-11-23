f17352eeacb66d5ca1236a3fa5ca411e539deb38
Robert Nagy <ronagy@icloud.com>
2018-11-23 16:11:41 +0100

    avcodec/aacenc: set pce value by options pce

--- libavcodec/aacenc.c
+++ libavcodec/aacenc.c
@@ -973,7 +973,7 @@ static av_cold int aac_encode_init(AVCodecContext *avctx)
     /* Channel map and unspecified bitrate guessing */
     s->channels = avctx->channels;
 
-    s->needs_pce = 1;
+    s->needs_pce = s->options.pce;
     for (i = 0; i < FF_ARRAY_ELEMS(aac_normal_chan_layouts); i++) {
         if (avctx->channel_layout == aac_normal_chan_layouts[i]) {
             s->needs_pce = s->options.pce;
