8ae57a7ab1aed7ca0b203d94be379976e42b7445
Niklas Haas <git@haasn.dev>
2025-02-18 18:47:37 +0100

    avfilter/alphadetect_vulkan: add alpha type detection filter
    This determines whether the input has any pixel values exceeding the alpha
    channel. In this case, we can be sure that the input is not premultiplied.
    In all other cases, the result in indeterminate. As such, we can only have
    false negatives, but no false positives.
    
    Signed-off-by: Niklas Haas <git@haasn.dev>
    Sponsored-by: nxtedition

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -178,6 +178,7 @@ extern const FFFilter ff_asink_anullsink;
 
 extern const FFFilter ff_vf_addroi;
 extern const FFFilter ff_vf_alphadet;
+extern const FFFilter ff_vf_alphadetect_vulkan;
 extern const FFFilter ff_vf_alphaextract;
 extern const FFFilter ff_vf_alphamerge;
 extern const FFFilter ff_vf_amplify;
