4ae4e30ae935cee52681a53cefab59da6c1240c8
Robert Nagy <ronagy@icloud.com>
2025-02-19 14:20:14 +0100

    Revert "avfilter: add blackdetect_cuda"
    This reverts commit 9ac459153b105e9990a362078d014d8f62387eac.

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -194,7 +194,6 @@ extern const FFFilter ff_vf_bilateral;
 extern const FFFilter ff_vf_bilateral_cuda;
 extern const FFFilter ff_vf_bitplanenoise;
 extern const FFFilter ff_vf_blackdetect;
-extern const FFFilter ff_vf_blackdetect_cuda;
 extern const FFFilter ff_vf_blackdetect_vulkan;
 extern const FFFilter ff_vf_blackframe;
 extern const FFFilter ff_vf_blend;
