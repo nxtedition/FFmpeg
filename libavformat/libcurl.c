622776f8de011eaaa722cf03d823e801156ee7a5
Niklas Haas <git@haasn.dev>
2026-07-09 16:23:43 +0200

    avformat/libcurl: early-exit on_done() when aborted
    Simplifies the following logic by not having to embed a check for !aborted
    in every conditional branch.
    
    Sponsored-by: nxtedition AB
    Signed-off-by: Niklas Haas <git@haasn.dev>

--- libavformat/libcurl.c
+++ libavformat/libcurl.c
@@ -477,14 +477,17 @@ static void on_done(CurlContext *c, CURLcode code)
         return;
     }
 
-    if (c->seek_queued && !aborted) {
+    if (aborted)
+        return;
+
+    if (c->seek_queued) {
         /* previous soft seek drain finished; can start new request now */
         c->seek_queued = 0;
         start_request(c);
         return;
     }
 
-    if (code == CURLE_OK && !aborted && c->stream_ok) {
+    if (code == CURLE_OK && c->stream_ok) {
         c->retry_count = 0;
         int64_t file_end = c->content_size > 0 ? c->content_size - 1 : -1;
         if (c->end_off > 0)
@@ -502,7 +505,7 @@ static void on_done(CurlContext *c, CURLcode code)
     }
 
     /* Resume seekable transfers after a recoverable error. */
-    if (!aborted && c->seekable && is_recoverable(code) &&
+    if (c->seekable && is_recoverable(code) &&
         c->retry_count < c->max_retries) {
         c->retry_count++;
         c->loop->num_retries++;
@@ -512,13 +515,12 @@ static void on_done(CurlContext *c, CURLcode code)
         return;
     }
 
-    if (!aborted) {
-        pthread_mutex_lock(&c->mutex);
-        if (!c->status)
-            c->status = curlcode_to_averror(code);
-        pthread_cond_broadcast(&c->cond);
-        pthread_mutex_unlock(&c->mutex);
-    }
+    /* Unhandled generic curl error */
+    pthread_mutex_lock(&c->mutex);
+    if (!c->status)
+        c->status = curlcode_to_averror(code);
+    pthread_cond_broadcast(&c->cond);
+    pthread_mutex_unlock(&c->mutex);
 }
 
 /* ------------------------------------------------------------------------- */
